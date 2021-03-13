30c3a8db50c4f21adc42d564fac10089f8689243
997try <997try@gmail.com>
2021-03-13 21:19:58 +0400

    init: tower-sort task

--- src/index.js
+++ src/index.js
@@ -2,5 +2,20 @@
 // You should implement your task here.
 
 module.exports = function towelSort (matrix) {
-  return [];
+  if(!matrix || matrix.length == 0){
+    return [];
+  }else{
+    let arr = [];
+    for (let i = 0; i < matrix.length; i++) {
+      if( i%2 !== 0){
+        matrix[i].reverse();
+      }
+    }
+    matrix.forEach(element => {
+      for (let j = 0; j < element.length; j++) {
+        arr.push(element[j]);
+      }
+    });
+    return arr;
+  }
 }
